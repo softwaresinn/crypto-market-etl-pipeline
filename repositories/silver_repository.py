b50eb5ca3af8ee37bde0a2ede534e87718c8cbce
softwaresinn <huzaifaahmad5535@gmail.com>
2026-08-08 15:06:46 +0500

    feat: Implementation of Medallion Arch.

---
+++ repositories/silver_repository.py
@@ -0,0 +1,77 @@
+import json
+from db.session import get_connection
+import transformations.bronze_to_silver
+
+
+# Insert transformations of bronze.metrics into silver.metrics.
+
+def insert_silver_metrics() -> None:
+    sql = """
+INSERT INTO silver.metrics (
+    cmc_id,
+    cmc_rank,
+    asset_name,
+    symbol,
+    infinite_supply,
+    circulating_supply,
+    total_supply,
+    max_supply,
+    self_reported_circulating_supply,
+    date_added,
+    last_updated,
+    tvl_ratio,
+    market_cap,
+    fully_diluted_market_cap,
+    market_cap_dominance,
+    self_reported_market_cap,
+    minted_market_cap,
+    price,
+    volume_24hr,
+    cex_volume_24h,
+    dex_volume_24h,
+    volume_pct_change_24h,
+	percent_change_24h,
+    percent_change_7d,
+    percent_change_30d,
+    percent_change_60d,
+    percent_change_90d
+)
+
+{transform_metrics}
+
+    """
+
+    conn = get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute("TRUNCATE TABLE silver.metrics")
+            cur.execute(sql)
+        conn.commit()
+    finally:
+        conn.close()
+
+
+# Insert transformation of Binance futures pairs from bronze.binance_future_pairs to silver layer
+
+def insert_silver_binance_future() -> None:
+    sql = """
+    INSERT INTO silver.binance_future_pairs(
+    symbol,
+    cmc_id,
+    base_asset,
+    underlying_type,
+    underlying_sub_type,
+    onboard_date
+    )   
+
+    {transform_binance_future_pairs}
+    """
+
+    conn = get_connection()
+    try:
+        with conn.cursor() as cur:
+            cur.execute("TRUNCATE TABLE silver.binance_future_pairs")
+            cur.execute(sql)
+        conn.commit()
+    finally:
+        conn.close()
--- transformations/bronze_to_silver.py
+++ transformations/bronze_to_silver.py
@@ -1,1 +1,59 @@
 
+# Transformation of bronze.metrics for curated layer (Silver)
+
+transform_binance_future_pairs="""SELECT
+b.raw_payload ->> 'symbol' AS symbols,
+cmc.cmc_id,
+b.raw_payload ->> 'baseAsset' AS base_asset,
+b.raw_payload ->> 'underlyingType' AS underlying_type,
+b.raw_payload -> 'underlyingSubType'->>0 AS underlying_sub_type,
+TO_TIMESTAMP((b.raw_payload ->> 'onboardDate')::BIGINT / 1000)::DATE AS onboard_date
+FROM bronze.binance_future_pairs b
+LEFT JOIN silver.metrics cmc 
+    ON UPPER(b.raw_payload->>'baseAsset') = UPPER(cmc.symbol)
+WHERE b.raw_payload->>'status' = 'TRADING' AND b.raw_payload->>'quoteAsset'='USDT'
+"""
+
+
+transform_metrics="""SELECT DISTINCT ON (raw_payload->>'symbol')
+
+    (raw_payload->>'id')::BIGINT,
+    (raw_payload->>'cmc_rank')::INT,
+    raw_payload->>'name',
+    raw_payload->>'symbol',
+    (raw_payload->>'infinite_supply')::BOOLEAN,
+    (raw_payload->>'circulating_supply')::NUMERIC,
+    (raw_payload->>'total_supply')::NUMERIC,
+    (raw_payload->>'max_supply')::NUMERIC,
+    (raw_payload->>'self_reported_circulating_supply')::NUMERIC,
+    (raw_payload->>'date_added')::TIMESTAMPTZ,
+    (raw_payload->>'last_updated')::TIMESTAMPTZ,
+    (raw_payload->>'tvl_ratio')::NUMERIC,
+
+    (raw_payload->'quote'->0->>'market_cap')::NUMERIC,
+    (raw_payload->'quote'->0->>'fully_diluted_market_cap')::NUMERIC,
+    (raw_payload->'quote'->0->>'market_cap_dominance')::NUMERIC,
+
+    (raw_payload->>'self_reported_market_cap')::NUMERIC,
+    (raw_payload->>'minted_market_cap')::NUMERIC,
+
+    (raw_payload->'quote'->0->>'price')::NUMERIC,
+    (raw_payload->'quote'->0->>'volume_24h')::NUMERIC,
+    (raw_payload->'quote'->0->>'cex_volume_24h')::NUMERIC,
+    (raw_payload->'quote'->0->>'dex_volume_24h')::NUMERIC,
+    (raw_payload->'quote'->0->>'volume_change_24h')::NUMERIC,	    
+	(raw_payload->'quote'->0->>'percent_change_24h')::NUMERIC,
+    (raw_payload->'quote'->0->>'percent_change_7d')::NUMERIC,
+    (raw_payload->'quote'->0->>'percent_change_30d')::NUMERIC,
+    (raw_payload->'quote'->0->>'percent_change_60d')::NUMERIC,
+    (raw_payload->'quote'->0->>'percent_change_90d')::NUMERIC
+
+FROM bronze.metrics
+ORDER BY
+    raw_payload->>'symbol',
+    (raw_payload->'quote'->0->>'market_cap')::NUMERIC DESC;
+
+
+"""
+
+
